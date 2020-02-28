2e53086eb23486ecdba9351dcd1f2c31716b6f71
l.schleyer <l.schleyer@netzhaut.de>
2020-02-28 14:45:14 +0100

    initial plugin upload

---
+++ craft/plugins/craft-prototype/src/Plugin.php
@@ -0,0 +1,11 @@
+<?php
+    namespace ls\prototype;
+
+   
+    /**
+     * User Guide Plugin
+     */
+    class Plugin extends \craft\base\Plugin
+    {
+        public $hasCpSection = true;
+    }
